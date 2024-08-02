a372b7f07b1f7f51536448c13c5482d4faf5ebed
Gui Silva <adrianon10@gmail.com>
2024-08-02 04:36:35 -0300

    feat: add slug and unique-entity-id value objects

---
+++ src/domain/entities/value-objects/__tests__/slug.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest'
+import { Slug } from '../slug'
+
+describe('Slug => Value Objects', () => {
+  it('should be able to create a new slug', async () => {
+    const sut = Slug.createFromText('Fake example slug')
+
+    expect(sut.value).toEqual('fake-example-slug')
+  })
+})
---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,30 @@
+
+export class Slug {
+  public readonly value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+
+  /**
+   * Receive a string and normalized it as a slug
+   * 
+   * Example: "An example title" => "an-example-title"
+   * 
+   * @param text {string}
+   */
+  public static createFromText(text: string) {
+    const slugText = text
+      .normalize("NFKD")
+      .toLocaleLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '')
+
+    return new Slug(slugText)
+  }
+
+}
