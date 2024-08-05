f84d440cbfbf17aa351a18641d4e92ad5c583f5b
Gui Silva <adrianon10@gmail.com>
2024-08-05 04:50:29 -0300

    feat: add create question use case

---
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -0,0 +1,33 @@
+import { Question } from '../../enterprise/entities/question'
+import { UniqueEntityID } from '../../enterprise/entities/value-objects/unique-entity-id'
+import { IQuestionsRepository } from '../repositories/questions-repository'
+
+interface ICreateQuestionUseCaseRequest {
+  author_id: string
+  title: string
+  content: string
+}
+
+interface ICreateQuestionUseCaseResponse {
+  question: Question
+}
+
+export class CreateQuestionUseCase {
+  constructor(private questionsRepository: IQuestionsRepository) {}
+
+  public async execute({
+    author_id,
+    title,
+    content,
+  }: ICreateQuestionUseCaseRequest): Promise<ICreateQuestionUseCaseResponse> {
+    const question = Question.create({
+      author_id: new UniqueEntityID(author_id),
+      title,
+      content,
+    })
+
+    await this.questionsRepository.create(question)
+
+    return { question }
+  }
+}
