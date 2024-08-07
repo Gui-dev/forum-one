8c77e0d27ace3f8ddabfeb5f8361a6168f86f50f
Gui Silva <adrianon10@gmail.com>
2024-08-07 03:28:34 -0300

    feat: add edit question use case

---
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -0,0 +1,38 @@
+import { IQuestionsRepository } from '../repositories/questions-repository'
+
+interface IEditQuestionUseCaseRequest {
+  author_id: string
+  question_id: string
+  title: string
+  content: string
+}
+
+interface IEditQuestionUseCaseResponse {}
+
+export class EditQuestionUseCase {
+  constructor(private questionsRepository: IQuestionsRepository) {}
+
+  public async execute({
+    author_id,
+    question_id,
+    title,
+    content,
+  }: IEditQuestionUseCaseRequest): Promise<IEditQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(question_id)
+
+    if (!question) {
+      throw new Error('Question not found')
+    }
+
+    if (author_id !== question.author_id.toString()) {
+      throw new Error('Not allowed')
+    }
+
+    question.title = title
+    question.content = content
+
+    await this.questionsRepository.save(question)
+
+    return {}
+  }
+}
