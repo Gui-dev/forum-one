ec6abde9668d4fb65bd0df8860ea783b896599e5
Gui Silva <adrianon10@gmail.com>
2024-08-09 04:50:40 -0300

    test: it should not be able to delete another user question comment

--- src/domain/forum/application/use-cases/__tests__/delete-question-comment.spec.ts
+++ src/domain/forum/application/use-cases/__tests__/delete-question-comment.spec.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from '@/domain/forum/enterprise/entities/value-objects/unique-entity-id'
 import { makeQuestionComment } from '@/test/factories/make-question-comment'
 import { InMemoryQuestionCommentsRepository } from '@/test/repositories/in-memory-question-comments-repository'
 
@@ -23,5 +24,20 @@ describe('Delete Question Comment Use Case', () => {
     })
 
     expect(inMemoryQuestionCommentsRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete another user question comment', async () => {
+    const questionComment = makeQuestionComment({
+      author_id: new UniqueEntityID('fake_author_id'),
+    })
+
+    await inMemoryQuestionCommentsRepository.create(questionComment)
+
+    await expect(() =>
+      sut.execute({
+        author_id: 'fake_another_author_id',
+        question_comment_id: questionComment.id.toString(),
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
