fdf2ca16c1787914fba843c58780925b35dcef49
Gui Silva <adrianon10@gmail.com>
2024-08-10 04:02:42 -0300

    test: it should be able to delete a answer comment and it should not be able to delete another user answer comment

---
+++ src/domain/forum/application/use-cases/__tests__/delete-answer-comment.spec.ts
@@ -0,0 +1,42 @@
+import { UniqueEntityID } from '@/domain/forum/enterprise/entities/value-objects/unique-entity-id'
+import { makeAnswerComment } from '@/test/factories/make-answer-comment'
+import { InMemoryAnswerCommentsRepository } from '@/test/repositories/in-memory-answer-comments-repository'
+
+import { DeleteAnswerCommentnUseCase } from '../delete-answer-comment'
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: DeleteAnswerCommentnUseCase
+
+describe('Delete Answer Comment Use Case', () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new DeleteAnswerCommentnUseCase(inMemoryAnswerCommentsRepository)
+  })
+  it('should be able to delete a answer comment', async () => {
+    const answerComment = makeAnswerComment()
+
+    await inMemoryAnswerCommentsRepository.create(answerComment)
+
+    await sut.execute({
+      author_id: answerComment.author_id.toString(),
+      answer_comment_id: answerComment.id.toString(),
+    })
+
+    expect(inMemoryAnswerCommentsRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete another user answer comment', async () => {
+    const answerComment = makeAnswerComment({
+      author_id: new UniqueEntityID('fake_author_id'),
+    })
+
+    await inMemoryAnswerCommentsRepository.create(answerComment)
+
+    await expect(() =>
+      sut.execute({
+        author_id: 'fake_another_author_id',
+        answer_comment_id: answerComment.id.toString(),
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
