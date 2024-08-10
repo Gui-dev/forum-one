7a4240fef15e8aad6c537f9eb02fd02a6d85dfb2
Gui Silva <adrianon10@gmail.com>
2024-08-10 04:01:24 -0300

    test: add helper methods findById and delete

--- src/test/repositories/in-memory-answer-comments-repository.ts
+++ src/test/repositories/in-memory-answer-comments-repository.ts
@@ -6,7 +6,24 @@ export class InMemoryAnswerCommentsRepository
   implements IAnswerCommentsRepository {
   public items: AnswerComment[] = []
 
+  public async findById(id: string): Promise<AnswerComment | null> {
+    const answerComment = this.items.find(
+      (item) => item.id.toString() === id,
+    )
+
+    if (!answerComment) {
+      return null
+    }
+
+    return answerComment
+  }
+
   public async create(answerComment: AnswerComment): Promise<void> {
     this.items.push(answerComment)
+  }
+
+  public async delete(answerComment: AnswerComment): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === answerComment.id)
+    this.items.splice(itemIndex, 1)
   }
 }
