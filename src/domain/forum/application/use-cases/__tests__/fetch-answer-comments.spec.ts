15705799ba7416bb9f1cce9c9549d890dff9ac08
Gui Silva <adrianon10@gmail.com>
2024-08-10 04:36:37 -0300

    test: it should be able to fetch answer comments and it should be able to fetch paginated answer commnets

---
+++ src/domain/forum/application/use-cases/__tests__/fetch-answer-comments.spec.ts
@@ -0,0 +1,58 @@
+import { UniqueEntityID } from '@/domain/forum/enterprise/entities/value-objects/unique-entity-id'
+import { makeAnswerComment } from '@/test/factories/make-answer-comment'
+import { InMemoryAnswerCommentsRepository } from '@/test/repositories/in-memory-answer-comments-repository'
+
+import { FetchAnswerCommentsUseCase } from '../fetch-answer-comments'
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: FetchAnswerCommentsUseCase
+
+describe('Fetch Answer Comments Use Case', () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new FetchAnswerCommentsUseCase(inMemoryAnswerCommentsRepository)
+  })
+  it('should be able to fetch answer comments', async () => {
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answer_id: new UniqueEntityID('fake_answer_id'),
+      }),
+    )
+
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answer_id: new UniqueEntityID('fake_answer_id'),
+      }),
+    )
+
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answer_id: new UniqueEntityID('fake_answer_id'),
+      }),
+    )
+
+    const { answer_comments } = await sut.execute({
+      answer_id: 'fake_answer_id',
+      page: 1,
+    })
+
+    expect(answer_comments).toHaveLength(3)
+  })
+
+  it('should be able to fetch paginated answer commnets', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryAnswerCommentsRepository.create(
+        makeAnswerComment({
+          answer_id: new UniqueEntityID('fake_answer_id'),
+        }),
+      )
+    }
+
+    const { answer_comments } = await sut.execute({
+      answer_id: 'fake_answer_id',
+      page: 2,
+    })
+
+    expect(answer_comments).toHaveLength(2)
+  })
+})
