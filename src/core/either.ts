1c28252d5ec890cd0ee24cb2e70e7c8bb984fce0
Gui Silva <adrianon10@gmail.com>
2024-08-12 03:37:00 -0300

    feat: add helper methods isLeft and isRight

--- src/core/either.ts
+++ src/core/either.ts
@@ -1,20 +1,36 @@
-export class Left<L> {
+export class Left<L, R> {
   public readonly value: L
 
   constructor(value: L) {
     this.value = value
   }
+
+  public isLeft(): this is Left<L, R> {
+    return true
+  }
+
+  public isRight(): this is Right<L, R> {
+    return false
+  }
 }
 
-export class Right<R> {
+export class Right<L, R> {
   public readonly value: R
 
   constructor(value: R) {
     this.value = value
   }
+
+  public isLeft(): this is Left<L, R> {
+    return false
+  }
+
+  public isRight(): this is Right<L, R> {
+    return true
+  }
 }
 
-export type Either<L, R> = Left<L> | Right<R>
+export type Either<L, R> = Left<L, R> | Right<L, R>
 
 export const left = <L, R>(value: L): Either<L, R> => {
   return new Left(value)
