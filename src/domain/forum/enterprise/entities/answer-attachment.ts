e3c836688f180d989be8ec2474ea8746ecb61e60
Gui Silva <adrianon10@gmail.com>
2024-08-14 04:53:59 -0300

    feat: add AnswerAttachment entity class

---
+++ src/domain/forum/enterprise/entities/answer-attachment.ts
@@ -0,0 +1,24 @@
+import { Entity } from '@/core/entities/entity'
+
+import { UniqueEntityID } from './value-objects/unique-entity-id'
+
+interface IAnswerAttachmentProps {
+  answer_id: UniqueEntityID
+  attachment_id: UniqueEntityID
+}
+
+export class AnswerAttachment extends Entity<IAnswerAttachmentProps> {
+  public static create(props: IAnswerAttachmentProps, id?: UniqueEntityID) {
+    const answerAttachment = new AnswerAttachment(props, id)
+
+    return answerAttachment
+  }
+
+  get answer_id() {
+    return this.props.answer_id
+  }
+
+  get attachment_id() {
+    return this.props.attachment_id
+  }
+}
