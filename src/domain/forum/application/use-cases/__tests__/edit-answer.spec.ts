0309d81699151468f0c6828dadf7bd554df9b0aa
Gui Silva <adrianon10@gmail.com>
2024-08-17 04:47:12 -0300

    test: it should be able to edit a answer -> add attachments

--- src/domain/forum/application/use-cases/__tests__/edit-answer.spec.ts
+++ src/domain/forum/application/use-cases/__tests__/edit-answer.spec.ts
@@ -1,51 +1,81 @@
 import { UniqueEntityID } from '@/domain/forum/enterprise/entities/value-objects/unique-entity-id'
 import { makeAnswer } from '@/test/factories/make-answer'
+import { makeAnswerAttachment } from '@/test/factories/make-answer-attachment'
+import { InMemoryAnswerAttachmentsRepository } from '@/test/repositories/in-memory-answer-attachements-repository'
 import { InMemoryAnswersRepository } from '@/test/repositories/in-memory-answers-repository'
 
 import { NotAllowedError } from '../../errors/not-allowed-error'
 import { EditAnswerUseCase } from '../edit-answer'
 
 let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
 let sut: EditAnswerUseCase
 
 describe('Edit Answer Use Case', () => {
   beforeEach(() => {
     inMemoryAnswersRepository = new InMemoryAnswersRepository()
-    sut = new EditAnswerUseCase(inMemoryAnswersRepository)
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository()
+    sut = new EditAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerAttachmentsRepository,
+    )
   })
   it('should be able to edit a answer', async () => {
-    const newQuestion = makeAnswer(
+    const newAnswer = makeAnswer(
       {
         author_id: new UniqueEntityID('fake_author_id'),
       },
       new UniqueEntityID('fake_answer_id'),
     )
 
-    await inMemoryAnswersRepository.create(newQuestion)
+    await inMemoryAnswersRepository.create(newAnswer)
+    inMemoryAnswerAttachmentsRepository.items.push(
+      makeAnswerAttachment({
+        answer_id: newAnswer.id,
+        attachment_id: new UniqueEntityID('1'),
+      }),
+      makeAnswerAttachment({
+        answer_id: newAnswer.id,
+        attachment_id: new UniqueEntityID('2'),
+      }),
+    )
+
     await sut.execute({
       author_id: 'fake_author_id',
-      question_id: newQuestion.id.toValue(),
+      answer_id: newAnswer.id.toValue(),
+      attachments_ids: ['1', '3'],
       content: 'Fake content',
     })
 
     expect(inMemoryAnswersRepository.items[0]).toMatchObject({
       content: 'Fake content',
     })
+    expect(
+      inMemoryAnswersRepository.items[0].attachments.currentItems,
+    ).toHaveLength(2)
+    expect(inMemoryAnswersRepository.items[0].attachments.currentItems).toEqual(
+      [
+        expect.objectContaining({ attachment_id: new UniqueEntityID('1') }),
+        expect.objectContaining({ attachment_id: new UniqueEntityID('3') }),
+      ],
+    )
   })
 
   it('should not be able to edit a answer from another user', async () => {
-    const newQuestion = makeAnswer(
+    const newAnswer = makeAnswer(
       {
         author_id: new UniqueEntityID('fake_author_id'),
       },
       new UniqueEntityID('fake_answer_id'),
     )
 
-    await inMemoryAnswersRepository.create(newQuestion)
+    await inMemoryAnswersRepository.create(newAnswer)
 
     const result = await sut.execute({
       author_id: 'faker_another_author_id',
-      question_id: newQuestion.id.toValue(),
+      answer_id: newAnswer.id.toValue(),
+      attachments_ids: ['1', '2'],
       content: 'Fake content',
     })
 
