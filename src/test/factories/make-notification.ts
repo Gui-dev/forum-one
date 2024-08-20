55f59d420406f88509466e52f7ee02c618a937ff
Gui Silva <adrianon10@gmail.com>
2024-08-20 04:40:57 -0300

    test: add make notification

---
+++ src/test/factories/make-notification.ts
@@ -0,0 +1,24 @@
+import { faker } from '@faker-js/faker'
+
+import { UniqueEntityID } from '@/domain/forum/enterprise/entities/value-objects/unique-entity-id'
+import {
+  INotificationProps,
+  Notification,
+} from '@/domain/notification/enterprise/entities/notification'
+
+export const makeNotification = (
+  override: Partial<INotificationProps> = {},
+  id?: UniqueEntityID,
+) => {
+  const notification = Notification.create(
+    {
+      recipient_id: new UniqueEntityID(),
+      title: faker.lorem.sentence(4),
+      content: faker.lorem.sentence(10),
+      ...override,
+    },
+    id,
+  )
+
+  return notification
+}
