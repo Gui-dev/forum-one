84c93738f1fd264fbdff72f3221064315c7f52fa
Gui Silva <adrianon10@gmail.com>
2024-08-20 04:00:41 -0300

    test: add helper methods findById and save

--- src/test/repositories/in-memory-notifications-repository.ts
+++ src/test/repositories/in-memory-notifications-repository.ts
@@ -5,7 +5,25 @@ import { Notification } from '@/domain/notification/enterprise/entities/notifica
 export class InMemoryNotificationsReposiotry
   implements INotificationsRepository {
   public items: Notification[] = []
+
+  public async findById(notification_id: string): Promise<Notification | null> {
+    const notification = this.items.find(
+      (item) => item.id.toString() === notification_id,
+    )
+
+    if (!notification) {
+      return null
+    }
+
+    return notification
+  }
+
   public async create(notification: Notification): Promise<void> {
     this.items.push(notification)
+  }
+
+  public async save(notification: Notification): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === notification.id)
+    this.items[itemIndex] = notification
   }
 }
